7de3e00d0c099799f7f64d6c80853f1b86f05783
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-09-27 11:08:32 +1000

    Add function docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,16 +1,18 @@
 def main():
-    """Ask the user for a password"""
+    """Ask the user for a password then print the length as asterisks."""
     minimum_password_length = 5
     password = get_password(minimum_password_length)
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Print the length of the password as asterisks."""
     for i in password:
         print("*", end="")
 
 
 def get_password(minimum_password_length):
+    """Get the password from the user with error checking."""
     password = input(f"Password (At least {minimum_password_length} characters): ")
     while len(password) < minimum_password_length:
         print(f"Password must be at least {minimum_password_length} characters")
