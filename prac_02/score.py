eafeaa6ca7df201dda56135cd4ddfd01f5408ba2
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-09-29 18:41:26 +1000

    Amend print statements in main to be more clear

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,25 +4,26 @@ Broken program to determine score status
 """
 from random import randint
 
+
 def main():
     """Get user score and display status. Then display result of random score."""
     score = int(input("Enter score: "))
     score_status = determine_score_result(score)
-    print(score_status)
-    random_score_status = determine_score_result((randint(0,100)))
-    print(random_score_status)
+    print("Your score status is: ", score_status)
+    random_score_status = determine_score_result((randint(0, 100)))
+    print("The random scores status is: ", random_score_status)
 
 
 def determine_score_result(score):
     """Determine score status."""
     if score < 0 or score > 100:
         return "Invalid score"
-    if score >= 90:
+    elif score >= 90:
         return "Excellent"
-    if score >= 50:
+    elif score >= 50:
         return "Passable"
     else:
         return "Bad"
 
 
-
+main()
