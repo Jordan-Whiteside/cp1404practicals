b2a31c6e689c29d1086c42e9aae4973b0dddb890
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-11 09:18:37 +1000

    Mark predictions of list output

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,13 +1,13 @@
 """Module 4 - Lists warmup"""
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-# numbers[0] = 3
-# numbers[-1] = 2
-# numbers[3] = 1
-# numbers[:-1] = 3
-# numbers[3:4] = 1, 5, 9, 2
-# 5 in numbers = True
-# 7 in numbers = False
-# "3" in numbers = False
-# numbers + [6, 5, 3] = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+# numbers[0] = 3 - Correct
+# numbers[-1] = 2 - Correct
+# numbers[3] = 1 - Correct
+# numbers[:-1] = 3 - Incorrect - [3, 1, 4, 1, 5, 9]
+# numbers[3:4] = 1, 5, 9, 2 - Incorrect - [1]
+# 5 in numbers = True - Correct
+# 7 in numbers = False - Correct
+# "3" in numbers = False - Correct
+# numbers + [6, 5, 3] = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 - Correct numbers (Should have brackets around list)
 
