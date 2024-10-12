e1a8e4f6aaf8a39c57253e0d53254505ebd85894
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-12 13:38:12 +1000

    Add function doc-strings and reformat to conform to PEP-8 standards for quick picks program in prac 4

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,25 +6,31 @@ MINIMUM_RANDOM_NUMBER = 1
 MAXIMUM_RANDOM_NUMBER = 45
 NUMBER_OF_RANDOM_NUMBERS_IN_LINE = 6
 
+
 def main():
+    """Generate a list of random numbers and display the list a chosen number of times."""
     number_of_quick_picks = int(input("How many quick picks? "))
     for i in range(number_of_quick_picks):
         quick_pick_numbers = []
         generate_quick_pick_numbers(quick_pick_numbers)
         display_quick_pick_numbers(quick_pick_numbers)
 
+
 def generate_quick_pick_numbers(quick_pick_numbers):
+    """Generate a random number and add it to a list that does not contain it already."""
     for i in range(NUMBER_OF_RANDOM_NUMBERS_IN_LINE):
         quick_pick_number = randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
         while quick_pick_number in quick_pick_numbers:
             quick_pick_number = randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
         quick_pick_numbers.append(quick_pick_number)
 
+
 def display_quick_pick_numbers(quick_pick_numbers):
+    """Display the numbers from a list in ascending order."""
     quick_pick_numbers.sort()
     for quick_pick_number in quick_pick_numbers:
         print(f"{quick_pick_number:2}", end=" ")
     print(sep="")
-    # print(f"{" ".join(sorted(str(quick_pick_number) for quick_pick_number in quick_pick_numbers)):2}")
+
 
 main()
