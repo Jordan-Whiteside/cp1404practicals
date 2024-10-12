e394135c08eca26f5fa0772a8f9b7e3fc2a7809a
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-12 11:36:02 +1000

    Add a check to see if the same number was picked for quick picks program in prac 4

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,13 +3,16 @@
 from random import randint
 
 MINIMUM_RANDOM_NUMBER = 1
-MAXIMUM_RANDOM_NUMBER = 45
+MAXIMUM_RANDOM_NUMBER = 6
+NUMBER_OF_RANDOM_NUMBERS_IN_LINE = 6
 
 number_of_quick_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_quick_picks):
     quick_pick_numbers = []
-    for j in range(number_of_quick_picks):
+    for j in range(NUMBER_OF_RANDOM_NUMBERS_IN_LINE):
         quick_pick_number = randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
+        while quick_pick_number in quick_pick_numbers:
+            quick_pick_number = randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
         quick_pick_numbers.append(quick_pick_number)
     print(quick_pick_numbers)
