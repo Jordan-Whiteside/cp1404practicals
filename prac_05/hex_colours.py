7a8979a1982d32e63b32624d133846d7992d6eb3
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-18 10:33:15 +1000

    Add dictionary of hex colours for hex colours program in prac 05

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,1 +1,8 @@
 """Prac_05 - Hex colours."""
+
+NAME_TO_CODE = {"FloralWhite": "#fffaf0", "Green Lizard": "#a7f432, ",
+                "Black": "#000000", "White": "#ffffff",
+                "VioletRed1": "#ff3e96", "Absolute Zero": "#0048ba",
+                "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                "Red1": "#ff0000", "Blue1": "#0000ff"}
+
