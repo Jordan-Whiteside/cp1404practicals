cf6729c9a55a51cce47ededf8a5dd933c07e6f49
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-18 10:17:38 +1000

    Refactor while loop error checking to use try except error handling for state name program in prac 05

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,16 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<3} is {name}")
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
+is_valid_input = False
+while not is_valid_input:
+    try:
+        state_code = input("Enter short state: ").upper()
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+        is_valid_input = True
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
