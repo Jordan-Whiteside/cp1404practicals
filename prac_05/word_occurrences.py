c561b770711501f31c37028888af0d1bbb8761c8
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-20 20:45:30 +1000

    Refactor to remove unnecessary code and add lower method to input for word occurrences program in prac 05

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,12 +5,12 @@ Actual: 41 minutes
 """
 from operator import itemgetter
 
-full_text = input("Text: ").strip()
+full_text = input("Text: ").lower().strip()
 word_to_count = {}
 words = full_text.split()
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
 max_length = max([len(key) for key in word_to_count.keys()])
-for word, count in sorted(word_to_count.items(), key=itemgetter(0)):
+for word, count in sorted(word_to_count.items()):
     print(f"{word:{max_length}} : {count}")
