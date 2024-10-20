88f233a9a1c87bf5f00fedcaec72ae6f774b9b62
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-20 18:13:09 +1000

    Rename constant in hex colours program in prac 05

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,6 +1,6 @@
 """Prac_05 - Hex colours."""
 
-NAME_TO_CODE = {"floralwhite": "#fffaf0", "green lizard": "#a7f432, ",
+COLOUR_TO_HEXADECIMAL = {"floralwhite": "#fffaf0", "green lizard": "#a7f432, ",
                 "black": "#000000", "white": "#ffffff",
                 "violetred1": "#ff3e96", "absolute zero": "#0048ba",
                 "amber": "#ffbf00", "amethyst": "#9966cc",
@@ -8,8 +8,8 @@ NAME_TO_CODE = {"floralwhite": "#fffaf0", "green lizard": "#a7f432, ",
 
 colour_name = input("Colour name? ").lower()
 while colour_name != "":
-    if colour_name in NAME_TO_CODE:
-        print(f"{colour_name} in hexadecimal is {NAME_TO_CODE[colour_name]}")
+    if colour_name in COLOUR_TO_HEXADECIMAL:
+        print(f"{colour_name} in hexadecimal is {COLOUR_TO_HEXADECIMAL[colour_name]}")
     else:
         print("Invalid colour name")
     colour_name = input("Colour name? ").lower()
