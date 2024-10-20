fba69f2554b6c0a09c982af0ffdbf7d5b6ef4c26
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-20 20:57:51 +1000

    Refactor wimbledon program in prac 05 to better follow style guide and PEP-8 standards

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,41 +5,46 @@ Actual: 2 hours 7 minutes
 """
 
 FILENAME = "wimbledon.csv"
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
 
 
 def main():
-    """Get a record of Wimbledon champions, process that into a dictionary and set then display them"""
-    records = get_records_from_file(FILENAME)
-    champion_to_win, country_codes = get_champion_wins_and_countries(records)
-    display_wimbledon_champions_and_countries(champion_to_win, country_codes)
+    """Get a record of Wimbledon champions, process that into a dictionary and set then display them."""
+    records = get_records(FILENAME)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
 
 
-def display_wimbledon_champions_and_countries(champion_to_win, country_code):
-    """Display wimbledon champions and there wins as well as what countries have won"""
+def display_results(champion_to_win, countries):
+    """Display wimbledon champions countries."""
     print("Wimbledon Champions")
-    for champion, win in champion_to_win.items():
-        print(f"{champion} {win}")
-    print(sep="")
-    print(f"These {len(country_code)} countries have won Wimbledon:")
-    print(", ".join(sorted(country_code)))
+    for champion, count in champion_to_win.items():
+        print(f"{champion} {count}")
+    print(f"\nThese {len(countries)} countries have won Wimbledon:")
+    print(", ".join(sorted(countries)))
 
 
-def get_champion_wins_and_countries(records):
-    """Create a dictionary with champion name paired with win(s) and countries code into a set"""
+def process_records(records):
+    """Create a dictionary of champion name paired with win(s) and set of countries from records."""
     champion_to_win = {}
+    countries = set()
     # record contains Year, Country, Champion, Country, Runner-up, Score in the final
     for record in records:
-        champion_to_win[record[2]] = champion_to_win.get(record[2], 0) + 1
-    country_code = {record[1] for record in records}
-    return champion_to_win, country_code
+        countries.add(record[INDEX_COUNTRY])
+        champion_to_win[record[INDEX_CHAMPION]] = champion_to_win.get(record[INDEX_CHAMPION], 0) + 1
+    return champion_to_win, countries
 
 
-def get_records_from_file(filename):
-    """Get records from a file returning a list of lists"""
+def get_records(filename):
+    """Get records from a file returning a list of lists."""
     with open(filename, "r", encoding="utf-8-sig") as in_file:
+        records = []
         # record fields Year, Country, Champion, Country, Runner-up, Score in the final
-        number_of_fields = in_file.readline().count(',')
-        records = [line.strip().split(',', number_of_fields) for line in in_file]
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(',')
+            records.append(parts)
     return records
 
 
