35f772a1f91c8d43df621eac5e900198e7739ca2
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-20 16:54:32 +1000

    Add display function for data and reformat code in wimbledon program for prac 05

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,30 +1,41 @@
 """
 Wimbledon
 Estimated: 1 hour
-Actual:
+Actual: 2 hours 7 minutes
 """
-
-from operator import itemgetter
 
 FILENAME = "wimbledon.csv"
 
 
 def main():
+    """Get a record of Wimbledon champions, process that into a dictionary and set then display them"""
     records = get_records_from_file(FILENAME)
-    print(records)
+    champion_to_win, country_codes = get_champion_wins_and_countries(records)
+    display_wimbledon_champions_and_countries(champion_to_win, country_codes)
 
 
-def create_champion_wins(records):
-    """Create a dictionary with champion name paired with win(s)"""
+def display_wimbledon_champions_and_countries(champion_to_win, country_code):
+    """Display wimbledon champions and there wins as well as what countries have won"""
+    print("Wimbledon Champions")
+    for champion, win in champion_to_win.items():
+        print(f"{champion} {win}")
+    print(sep="")
+    print(f"These {len(country_code)} countries have won Wimbledon:")
+    print(", ".join(sorted(country_code)))
+
+
+def get_champion_wins_and_countries(records):
+    """Create a dictionary with champion name paired with win(s) and countries code into a set"""
     champion_to_win = {}
+    # record contains Year, Country, Champion, Country, Runner-up, Score in the final
     for record in records:
         champion_to_win[record[2]] = champion_to_win.get(record[2], 0) + 1
-    return champion_to_win
+    country_code = {record[1] for record in records}
+    return champion_to_win, country_code
 
 
 def get_records_from_file(filename):
     """Get records from a file returning a list of lists"""
-    records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         # record fields Year, Country, Champion, Country, Runner-up, Score in the final
         number_of_fields = in_file.readline().count(',')
