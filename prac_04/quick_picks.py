84a155fe255531c3fed5705c6bfb684952e6a465
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-29 15:20:57 +1000

    Change generate quick pick numbers to a better function design in prac 05

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -11,18 +11,19 @@ def main():
     """Generate a list of random numbers and display the list a chosen number of times."""
     number_of_quick_picks = int(input("How many quick picks? "))
     for i in range(number_of_quick_picks):
-        quick_pick_numbers = []
-        generate_quick_pick_numbers(quick_pick_numbers)
+        quick_pick_numbers = generate_quick_pick_numbers()
         display_quick_pick_numbers(quick_pick_numbers)
 
 
-def generate_quick_pick_numbers(quick_pick_numbers):
+def generate_quick_pick_numbers():
     """Generate a random number and add it to a list that does not contain it already."""
+    quick_pick_numbers = []
     for i in range(NUMBER_OF_RANDOM_NUMBERS_IN_LINE):
         quick_pick_number = randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
         while quick_pick_number in quick_pick_numbers:
             quick_pick_number = randint(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER)
         quick_pick_numbers.append(quick_pick_number)
+    return quick_pick_numbers
 
 
 def display_quick_pick_numbers(quick_pick_numbers):
