2c5c60e600c21260f8229cf997673336d48e4319
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-30 11:27:02 +1000

    Complete guitar test program in prac 06

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,2 +1,11 @@
 """Guitar test program."""
+from prac_06.guitar import Guitar
 
+gibsion = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another guitar", 2013, 100.0)
+
+
+print(f"Gibson L-5 CES get_age() - Expected 102. Got {gibsion.get_age()}")
+print(f"Another Guitar get_age() - Expected 11. Got {another_guitar.get_age()}")
+print(f"Gibson L-5 CES is_vintage() - Expected True. Got {gibsion.is_vintage()}")
+print(f"Another Guitar is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
