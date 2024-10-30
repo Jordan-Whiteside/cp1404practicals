aa46ea84a2ca206f4ae4dd5fc910b1dffff614b1
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-30 10:30:26 +1000

    Add starting code for a class as well as string and is dynamic methods to programming language class in prac 06

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,1 +1,14 @@
 """Class programming languages"""
+
+class ProgrammingLanguage:
+    def __init__(self, language="", typing="", reflection="", year=""):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
