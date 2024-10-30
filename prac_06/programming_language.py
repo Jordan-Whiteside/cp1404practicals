748ec5ca9e8a1c7f6abbd0e4aabfe0644c332729
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-10-30 11:16:37 +1000

    Change class fields (attributes) to their type for programming language class in prac 06

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,11 +1,12 @@
 """Class programming languages"""
 
+
 class ProgrammingLanguage:
-    def __init__(self, language="", typing="", reflection="", year=""):
+    def __init__(self, language, typing, reflection, year=0):
         self.language = language
-        self.typing = typing
-        self.reflection = reflection
-        self.year = year
+        self.typing = typing.title()
+        self.reflection = bool(reflection)
+        self.year = int(year)
 
     def __str__(self):
         return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
