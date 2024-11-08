25e3f92e8860eb8580340fc65da63c4490a1c1f8
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-11-09 09:16:25 +1000

    Complete first part of More Guitars exercise for prac 07

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,1 +1,25 @@
 """My Guitars program."""
+
+from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+
+def main():
+    guitars = load_file(FILENAME)
+    for guitar in guitars:
+        print(guitar)
+
+
+def load_file(filename):
+    record = []
+    # Each line in file contains the fields name,year,cost
+    in_file = open(filename)
+    for line in in_file:
+        parts = line.strip().split(',')
+        record.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
+    in_file.close()
+    return record
+
+
+main()
