6bf15b98c33f456fa93932e1ffaef16b14317475
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-11-09 11:31:56 +1000

    Add completed load projects function to project management program in prac 07

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,7 @@ Project management program.
 Estimated: 5 hours
 Actual:
 """
+from prac_07.project import Project
 
 DEFAULT_FILE = "projects.txt"
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
@@ -10,11 +11,14 @@ MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter p
 
 def main():
     """Load file then display menu. Will save file after quiting"""
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects(DEFAULT_FILE)
+    print(f"Loaded {len(projects)} from {DEFAULT_FILE}")
     print(MENU)
     choice = input(">>>").upper()
     while choice != "Q":
         if choice == "L":
-            print("Load projects")
+            print("Load from new file")
         elif choice == "S":
             print("Save projects")
         elif choice == "D":
@@ -30,6 +34,17 @@ def main():
         print(MENU)
         choice = input(">>>").upper()
     print("Quit")
+
+
+def load_projects(filename):
+    records = []
+    with open(filename) as in_file:
+        in_file.readline()  #
+        for line in in_file:
+            parts = line.strip().split("\t")
+            record = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+            records.append(record)
+    return records
 
 
 main()
