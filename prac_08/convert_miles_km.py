40cb057b966bdc620c30d06da420a24390a3e8ec
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-11-19 16:43:54 +1000

    Add handle calculate function to ConvertMilesKm class and change associated kivy to update automatically on text input

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,6 +4,7 @@ from kivy.properties import StringProperty
 
 CONVERSION_MILES_TO_KM = 1.60934
 
+
 class ConvertMilesKm(App):
     km_output = StringProperty()
 
@@ -17,15 +18,21 @@ class ConvertMilesKm(App):
     def handle_increment(self, increment):
         """Handle and increment change of up or down."""
         miles = self.get_valid_miles() + increment
-        self.root.ids.input_number.text = str(miles)
+        self.root.ids.input_miles.text = str(miles)
+
+    def handle_calculate(self):
+        """Handle Calculating miles displaying in a label widget."""
+        miles = self.get_valid_miles()
+        kilometers = miles * CONVERSION_MILES_TO_KM
+        self.root.ids.output_label.text = str(kilometers)
 
     def get_valid_miles(self):
-        """Get a valid mile."""
+        """Get a valid mile using exception handling."""
         try:
-            mile = float(self.root.ids.input_number.text)
+            mile = float(self.root.ids.input_miles.text)
             return mile
         except ValueError:
-            pass
+            return 0.0
 
 
 ConvertMilesKm().run()
