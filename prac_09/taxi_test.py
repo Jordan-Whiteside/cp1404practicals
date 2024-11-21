eb0b422e13f38800c77490c16f655439c74b3de2
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-11-22 09:38:07 +1000

    Update to reflect change in taxi class for prac 09

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,7 +2,7 @@
 
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 print(my_taxi.drive(40))
 print(my_taxi, f"Current fare ${my_taxi.get_fare()}")
 my_taxi.start_fare()
