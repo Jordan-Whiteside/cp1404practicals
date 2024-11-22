ff6c6962632f10c558b37dd9e816b58b4f9cd6a2
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-11-22 10:53:08 +1000

    Add assert statements to test silver service taxi class for prac 09

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,6 +4,7 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 my_taxi = SilverServiceTaxi("Hummer", 200, 2)
 
-print(my_taxi.drive(18))
+assert my_taxi.drive(18) == 18
 print(my_taxi.get_fare())
+assert my_taxi.get_fare() == 48.8
 # assert my_taxi.drive(18)
