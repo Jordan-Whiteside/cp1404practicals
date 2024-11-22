4d18c54612c044a62c58bb17c65a308c7fc11f43
Jordan <jordan.whiteside@my.jcu.edu.au>
2024-11-22 10:02:23 +1000

    Add unreliable car test program to prac 09

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,7 @@
+"""unreliable car test."""
+
+from prac_09.unreliable_car import UnreliableCar
+
+my_unreliable_car = UnreliableCar("Shit box", 1000, 50)
+for i in range(100):
+    print(my_unreliable_car.drive(10))
